033df8002802dcd3dbb3127a87a5964b976064e0
StevenD'Hondt <steven.dhondt@kenze.be>
2021-02-22 12:10:09 +0100

    :alembic: Rehearsing

--- src/be/dog/d/steven/exam1Z0_816and1Z0_817/chapter4/FunctionalProgramming.java
+++ src/be/dog/d/steven/exam1Z0_816and1Z0_817/chapter4/FunctionalProgramming.java
@@ -2,6 +2,8 @@ package be.dog.d.steven.exam1Z0_816and1Z0_817.chapter4;
 
 import java.time.LocalDate;
 import java.util.HashMap;
+import java.util.Optional;
+import java.util.Random;
 import java.util.function.*;
 
 public class FunctionalProgramming {
@@ -75,5 +77,53 @@ public class FunctionalProgramming {
         Function<Integer, Integer> next = x -> x * 2;
         Function<Integer, Integer> both = next.compose(first);
         System.out.println(both.apply(3));
+    }
+}
+
+// OPTIONAL
+
+class OptionalExample {
+    public static Optional<Double> average1(int... scores) {
+        if (scores.length == 0) {
+            return Optional.empty();
+        }
+        int sum = 0;
+        for (int score : scores) {
+            sum += score;
+        }
+        return Optional.of((double) sum / scores.length);
+    }
+
+    public static Double average2(int... scores) {
+        if (scores.length == 0) {
+            return null;
+        }
+        int sum = 0;
+        for (int score : scores) {
+            sum += score;
+        }
+        return (double) sum / scores.length;
+    }
+
+    public static void main(String[] args) {
+        Optional<Double> average1 = OptionalExample.average1();
+        Optional<Double> average2 = OptionalExample.average1(6, 14);
+        if (average1.isPresent()) {  // Check
+            System.out.println(average1.get());
+        }
+        average2.ifPresent(System.out::println);  // Functional expression
+
+        Optional<Double> average3 = Optional.ofNullable(OptionalExample.average2());
+        Optional<Double> average4 = Optional.ofNullable(OptionalExample.average2(9, 18));
+
+        average4.ifPresent(System.out::println);
+        System.out.println(average3.orElse(Double.NaN));
+        System.out.println(average3.orElseGet(() -> Math.random() * 100));
+        System.out.println(average3.orElseGet(Math::random) * 100);
+        System.out.println(average3.orElseGet(() -> new Random().nextDouble() * 100));
+        System.out.println(average3.orElseThrow());
+        System.out.println(average3.orElseThrow(() -> new IllegalStateException()));
+        System.out.println(average3.orElseThrow(IllegalStateException::new));
+
     }
 }
