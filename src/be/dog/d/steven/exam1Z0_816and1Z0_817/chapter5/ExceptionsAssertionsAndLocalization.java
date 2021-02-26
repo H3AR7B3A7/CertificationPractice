40557c086fea5345049811cc6a5588b475a6f495
StevenD'Hondt <steven.dhondt@kenze.be>
2021-02-26 15:49:46 +0100

    :alembic: Rehearsing

--- src/be/dog/d/steven/exam1Z0_816and1Z0_817/chapter5/ExceptionsAssertionsAndLocalization.java
+++ src/be/dog/d/steven/exam1Z0_816and1Z0_817/chapter5/ExceptionsAssertionsAndLocalization.java
@@ -1,5 +1,10 @@
 package be.dog.d.steven.exam1Z0_816and1Z0_817.chapter5;
 
+import java.text.NumberFormat;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 class CannotSwimException extends Exception { }
 
 class DangerInTheWaterException extends RuntimeException { }
@@ -90,7 +95,7 @@ class UsingMyFileReader {
             System.out.println("Finally Block");
         }
         MyFileReader myFileReader1 = new MyFileReader("first"); // Needs to be effectively final
-        MyFileReader myFileReader2 = new MyFileReader("second");
+        MyFileReader myFileReader2 = new MyFileReader("second"); // If this line throws an exception the previous will never be closed
         try (myFileReader1; myFileReader2){
             System.out.println("Try Block");
         } finally {
@@ -99,3 +104,70 @@ class UsingMyFileReader {
     }
 }
 
+// DATES AND TIMES
+
+class DateAndTimeExamples {
+    public static void main(String[] args) {
+        System.out.println(LocalDate.now());
+        System.out.println(LocalTime.now());
+        System.out.println(LocalDateTime.now());
+        System.out.println(ZonedDateTime.now());
+
+        LocalTime thisTime = LocalTime.of(15,14,10);
+        System.out.println(thisTime.getHour());
+        LocalDate today = LocalDate.of(2021, Month.FEBRUARY, 26);
+        System.out.println(today.getDayOfWeek());
+        System.out.println(today.getDayOfYear());
+
+        LocalDateTime now = LocalDateTime.of(today, thisTime);
+        LocalDateTime tomorrow = now.plusDays(1);
+
+        LocalDateTime someDay = LocalDateTime.of(2021,3, 30, 15, 20);
+
+        System.out.println(someDay.format(DateTimeFormatter.ISO_LOCAL_DATE));
+        System.out.println(someDay.format(DateTimeFormatter.ISO_LOCAL_TIME));
+
+        var myFormat = DateTimeFormatter.ofPattern("MMMM dd, yyyy 'at' hh:mm");
+        System.out.println(myFormat.format(someDay));
+        System.out.println(someDay.format(myFormat));
+    }
+}
+
+// INTERNATIONALIZATION
+
+class InternationalizationExamples {
+    private static int someLargeNumber = 1_432_200;
+    private static double money = 2.45;
+
+    public static void main(String[] args) {
+        Locale locale = Locale.getDefault();
+        System.out.println(locale);
+
+        Locale myLocale = new Locale.Builder()
+                .setLanguage("nl")
+                .setRegion("BE")
+                .build();
+        System.out.println(myLocale);
+
+        Locale.setDefault(myLocale);
+        System.out.println(Locale.getDefault());
+
+        var us = NumberFormat.getInstance(Locale.US);
+        var ge = NumberFormat.getInstance(Locale.GERMANY);
+        var ca = NumberFormat.getInstance(Locale.CANADA_FRENCH);
+
+        var usC = NumberFormat.getCurrencyInstance(Locale.US);
+        var geC = NumberFormat.getCurrencyInstance(Locale.GERMANY);
+        var caC = NumberFormat.getCurrencyInstance(Locale.CANADA_FRENCH);
+
+        System.out.println(us.format(someLargeNumber));
+        System.out.println(ge.format(someLargeNumber));
+        System.out.println(ca.format(someLargeNumber));
+
+        System.out.println(usC.format(money));
+        System.out.println(geC.format(money));
+        System.out.println(caC.format(money));
+
+
+    }
+}
