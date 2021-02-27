42adf767774d78a76af79159a778338d9a3ddaac
StevenD'Hondt <steven.dhondt@kenze.be>
2021-02-27 18:23:07 +0100

    :alembic: Rehearsing

--- src/be/dog/d/steven/exam1Z0_816and1Z0_817/chapter5/ExceptionsAssertionsAndLocalization.java
+++ src/be/dog/d/steven/exam1Z0_816and1Z0_817/chapter5/ExceptionsAssertionsAndLocalization.java
@@ -2,11 +2,14 @@ package be.dog.d.steven.exam1Z0_816and1Z0_817.chapter5;
 
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
+import java.text.MessageFormat;
 import java.text.NumberFormat;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.Locale;
+import java.util.Properties;
+import java.util.ResourceBundle;
 
 class CannotSwimException extends Exception {
 }
@@ -207,12 +210,12 @@ class DateTimeFormatterExamples {
         System.out.println(dtf.withLocale(locale).format(dateTime));
     }
 
-    public static void printCurrency(Locale locale, double amount){
+    public static void printCurrency(Locale locale, double amount) {
         System.out.println(NumberFormat.getCurrencyInstance(locale).format(amount) +
                 ", " + locale.getDisplayLanguage());
     }
 
-    public static void printCurrencyDefault(Locale locale, double amount){
+    public static void printCurrencyDefault(Locale locale, double amount) {
         System.out.println(NumberFormat.getCurrencyInstance().format(amount) +
                 ", " + locale.getDisplayLanguage());
     }
@@ -224,19 +227,87 @@ class DateTimeFormatterExamples {
         print(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG), LocalDateTime.now());
         print(DateTimeFormatter.ISO_ORDINAL_DATE, LocalDateTime.now(), Locale.CANADA_FRENCH);
 
-        printCurrency(new Locale("en","US"),123.45);
-        printCurrency(new Locale("es","ES"),123.45);
-        printCurrency(new Locale("fr","CA"),123.45);
+        printCurrency(new Locale("en", "US"), 123.45);
+        printCurrency(new Locale("es", "ES"), 123.45);
+        printCurrency(new Locale("fr", "CA"), 123.45);
 
-        Locale.setDefault(new Locale("en","US"));
+        Locale.setDefault(new Locale("en", "US"));
         printCurrencyDefault(new Locale("en", "US"), 123.45);
-        Locale.setDefault(Locale.Category.DISPLAY, new Locale("es","ES"));
+        Locale.setDefault(Locale.Category.DISPLAY, new Locale("es", "ES"));
         printCurrencyDefault(new Locale("es", "ES"), 123.45);
 
-        Locale.setDefault(new Locale("en","US"));
-        Locale.setDefault(Locale.Category.FORMAT, new Locale("es","ES"));
+        Locale.setDefault(new Locale("en", "US"));
+        Locale.setDefault(Locale.Category.FORMAT, new Locale("es", "ES"));
         printCurrencyDefault(new Locale("es", "ES"), 123.45);
-        Locale.setDefault(Locale.Category.DISPLAY, new Locale("es","ES"));
+        Locale.setDefault(Locale.Category.DISPLAY, new Locale("es", "ES"));
         printCurrencyDefault(new Locale("es", "ES"), 123.45);
+    }
+}
+
+// RESOURCE BUNDLES
+
+class ResourceBundleExample {
+    public static void printWelcome(Locale locale) {
+        ResourceBundle rb = ResourceBundle.getBundle("be.dog.d.steven.exam1Z0_816and1Z0_817.chapter5.Zoo", locale);
+        System.out.println(
+                rb.getString("hello").substring(0, 1).toUpperCase() + rb.getString("hello").substring(1)
+                        + ", " + rb.getString("open")
+        );
+    }
+
+    public static void main(String[] args) {
+        printWelcome(new Locale("en"));
+        // Country specification will only be used if it is present, if it is not the bundle will be specified by language
+        printWelcome(new Locale("fr", "FR"));
+
+        ResourceBundle rb1 = ResourceBundle.getBundle("be.dog.d.steven.exam1Z0_816and1Z0_817.chapter5.Zoo", new Locale("fr"));
+        rb1.keySet().stream()
+                .map(k -> k + ": " + rb1.getString(k))
+                .forEach(System.out::println);
+        ResourceBundle rb2 = ResourceBundle.getBundle("be.dog.d.steven.exam1Z0_816and1Z0_817.chapter5.Zoo"); // Default is used
+        rb2.keySet().stream()
+                .map(k -> k + ": " + rb2.getString(k))
+                .forEach(System.out::println);
+        // Default locale will be used when only GE specific bundle is present and we look for bundle by the language only
+        ResourceBundle rb3 = ResourceBundle.getBundle("be.dog.d.steven.exam1Z0_816and1Z0_817.chapter5.Zoo", new Locale("ge"));
+        rb3.keySet().stream()
+                .map(k -> k + ": " + rb3.getString(k))
+                .forEach(System.out::println);
+        ResourceBundle rb4 = ResourceBundle.getBundle("be.dog.d.steven.exam1Z0_816and1Z0_817.chapter5.Zoo", new Locale("ge","GE"));
+        rb4.keySet().stream()
+                .map(k -> k + ": " + rb4.getString(k))
+                .forEach(System.out::println);
+        // ... so creating resources by languages before creating them by country/zone is a good idea.
+        Locale.setDefault(new Locale("ch"));
+        // When language is not found, and default locale language isn't found either, the default bundle will be used
+        ResourceBundle rb5 = ResourceBundle.getBundle("be.dog.d.steven.exam1Z0_816and1Z0_817.chapter5.Zoo", new Locale("ru"));
+        rb5.keySet().stream()
+                .map(k -> k + ": " + rb5.getString(k))
+                .forEach(System.out::println);
+        // ... so creating a default bundle is a good idea.
+
+        System.out.println(rb1.getString("default")); // Will look in parent files if key is not present (zone > lang > default bundle)
+
+        String format = rb1.getString("formatted_string");
+        System.out.println(MessageFormat.format(format,"Steven", "Belgium"));
+        System.out.println(MessageFormat.format(format,"Steven"));
+    }
+}
+
+// PROPERTIES
+
+class PropertiesExample {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.setProperty("name","Steven");
+        props.setProperty("age","31");
+        props.put("faulty_age",31); // We can use put() to add non-strings, but this will not not work as expected
+
+        System.out.println(props.getProperty("name"));
+        System.out.println(props.getProperty("camel"));
+        System.out.println(props.getProperty("camel", "default")); // getProperty() can take a default
+        System.out.println(props.get("camel")); // get() can't take a default value
+        System.out.println(props.getProperty("faulty_age")); // We can't get non-string values with getProperties()
+        System.out.println(props.get("faulty_age")); // We can get non-strings with the get() function
     }
 }
