05e2a94679f7c02d1da9b3a0077b04fdfb25dccd
StevenD'Hondt <steven.dhondt@kenze.be>
2021-03-02 22:17:47 +0100

    :alembic: Rehearsing

--- src/be/dog/d/steven/exam1Z0_816and1Z0_817/chapter7/Concurrency.java
+++ src/be/dog/d/steven/exam1Z0_816and1Z0_817/chapter7/Concurrency.java
@@ -4,6 +4,7 @@ package be.dog.d.steven.exam1Z0_816and1Z0_817.chapter7;
 
 import java.util.List;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 class PrintData implements Runnable { // Implementing Runnable
     @Override
@@ -88,6 +89,8 @@ class Single {
     }
 }
 
+// FUTURE
+
 class SingleFuture {
     private static int COUNTER = 0;
 
@@ -123,6 +126,8 @@ class SingleFuture {
     }
 }
 
+// WAITING FOR RESULTS
+
 class FutureExample {
     public static void main(String[] args) throws Exception {
         ExecutorService executorService = null;
@@ -138,23 +143,94 @@ class FutureExample {
     }
 }
 
+// TASK COLLECTIONS
+
 class ListOfFutures {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         ExecutorService executorService = null;
         System.out.println("START");
         Callable<String> task = () -> "bla";
         try {
             executorService = Executors.newSingleThreadExecutor();
             List<Future<String>> list = executorService.invokeAll(List.of(task, task, task)); // Executed synchronously
-            for (Future<String> future: list){
+            for (Future<String> future : list) {
                 System.out.println(future.get());
             }
-        }finally {
-            if(executorService != null){
+        } finally {
+            if (executorService != null) {
                 executorService.shutdown();
             }
         }
         System.out.println("END");
+    }
+}
+
+// SCHEDULING
+
+class Schedule {
+    public static void main(String[] args) throws Exception {
+        ScheduledExecutorService executorService = null;
+        System.out.println("START");
+        Callable<String> task = () -> "bla";
+        ScheduledFuture<String> result;
+        try {
+            executorService = Executors.newSingleThreadScheduledExecutor();
+            result = executorService.schedule(task, 10, TimeUnit.SECONDS); // Executed synchronously
+            System.out.println(result.getDelay(TimeUnit.SECONDS));
+        } finally {
+            if (executorService != null) {
+                executorService.shutdown();
+            }
+        }
+        System.out.println("END");
+        Thread.sleep(5000);
+        System.out.println(result.getDelay(TimeUnit.SECONDS));
+    }
+}
+
+class Schedule2 {
+    public static void main(String[] args) throws Exception {
+        ScheduledExecutorService executorService = null;
+        System.out.println("START");
+        Runnable task = () -> System.out.println("Bla");
+        ScheduledFuture<?> result;
+        try {
+            executorService = Executors.newSingleThreadScheduledExecutor();
+            result = executorService.scheduleAtFixedRate(task, 0, 1, TimeUnit.SECONDS); // Executed synchronously
+            System.out.println(result.getDelay(TimeUnit.SECONDS));
+        } finally {
+            // Not closing thread
+        }
+        System.out.println("END");
+        Thread.sleep(9000);
+        System.out.println(result.getDelay(TimeUnit.MILLISECONDS) + "ms");
+        executorService.shutdown();
+        System.out.println(result.getDelay(TimeUnit.MILLISECONDS) + "ms");
+    }
+}
+
+// THREAD POOLS
+
+class SheepManager {
+    private final AtomicInteger sheepCount = new AtomicInteger(0);
+
+    private void incrementAndReport() {
+        System.out.println(sheepCount.incrementAndGet() + " "); // No data will be lost, but will not print in order
+    }
+
+    public static void main(String[] args) {
+        ExecutorService executorService = null;
+        try {
+            executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+            SheepManager manager = new SheepManager();
+            for (int i = 0; i < 10; i++) {
+                executorService.submit(manager::incrementAndReport);
+            }
+        } finally {
+            if (executorService != null) {
+                executorService.shutdown();
+            }
+        }
     }
 }
 
