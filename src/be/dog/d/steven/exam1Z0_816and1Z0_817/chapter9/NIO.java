c590fa637bef65bd07f72a759387b9ba0cda8f24
StevenD'Hondt <steven.dhondt@kenze.be>
2021-03-08 18:06:04 +0100

    :alembic: Rehearsing

--- src/be/dog/d/steven/exam1Z0_816and1Z0_817/chapter9/NIO.java
+++ src/be/dog/d/steven/exam1Z0_816and1Z0_817/chapter9/NIO.java
@@ -2,23 +2,36 @@ package be.dog.d.steven.exam1Z0_816and1Z0_817.chapter9;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+
+// PATH
 
 public class NIO {
     public static void main(String[] args) {
+        // Path
         Path path1 = Path.of("src/be/dog/d/steven/exam1Z0_816and1Z0_817/chapter9/files/File1.txt");
+
+        // Paths
+        Path path2 = Paths.get("src/be/dog/d/steven/exam1Z0_816and1Z0_817/chapter9/files/File1.txt");
+
+        // URI
         URI uri1 = null;
         try {
             uri1 = new URI("file://src/be/dog/d/steven/exam1Z0_816and1Z0_817/chapter8/files/File1.txt");
         } catch (URISyntaxException e) {
             e.printStackTrace();
         }
-
         URI uri2 = path1.toUri();
+        Path path3;
         if(uri1 != null) {
-            Path path2 = Path.of(uri1);
+            path3 = Path.of(uri1);
         }
 
-        System.out.println(uri2);
+        // FileSystem
+        Path path4 = FileSystems.getDefault().getPath("src/be/dog/d/steven/exam1Z0_816and1Z0_817/chapter9/files/File1.txt");
+
+
     }
 }
