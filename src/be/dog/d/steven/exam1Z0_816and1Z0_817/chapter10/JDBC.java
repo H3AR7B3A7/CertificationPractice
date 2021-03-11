1d743cd2533f47f086531aa817c58ee415651a16
StevenD'Hondt <steven.dhondt@kenze.be>
2021-03-11 18:51:23 +0100

    :alembic: Rehearsing

--- src/be/dog/d/steven/exam1Z0_816and1Z0_817/chapter10/JDBC.java
+++ src/be/dog/d/steven/exam1Z0_816and1Z0_817/chapter10/JDBC.java
@@ -1,6 +1,9 @@
 package be.dog.d.steven.exam1Z0_816and1Z0_817.chapter10;
 
 import java.sql.*;
+import java.util.Arrays;
+
+// EXAMPLE
 
 public class JDBC {
     public static void main(String[] args) throws SQLException {
@@ -16,6 +19,8 @@ public class JDBC {
     }
 }
 
+// CONNECTION
+
 class Connect {
     public static void main(String[] args) throws SQLException {
         Connection conn = DriverManager.getConnection("jdbc:derby:zoo");
@@ -23,11 +28,13 @@ class Connect {
     }
 }
 
+// EXECUTE QUERY
+
 class Prepared {
     public static void main(String[] args) throws SQLException {
         Connection conn = DriverManager.getConnection("jdbc:derby:zoo");
         try (var ps = conn.prepareStatement("SELECT * FROM exhibits");
-             ResultSet rs = ps.executeQuery()){
+             ResultSet rs = ps.executeQuery()) {
             while (rs.next())
                 System.out.println(rs.getString(1) + ", "
                         + rs.getString(2) + ", "
@@ -36,6 +43,8 @@ class Prepared {
     }
 }
 
+// EXECUTE PREPARED STATEMENTS
+
 class Executions {
     private static final String URL = "jdbc:derby:zoo";
 
@@ -43,7 +52,11 @@ class Executions {
     private final static String UPDATE = "UPDATE exhibits SET name = 'Bambi' WHERE name = 'Deer'";
     private final static String DELETE = "DELETE FROM exhibits WHERE id = 10";
 
+    private final static String SELECT = "SELECT name FROM names";
+
     public static void main(String[] args) throws SQLException {
+
+        // EXECUTE UPDATE
 
         try (Connection conn = DriverManager.getConnection(URL);
              PreparedStatement ps = conn.prepareStatement(
@@ -64,6 +77,53 @@ class Executions {
                      DELETE)) {
             int result = ps.executeUpdate();
             System.out.println("Rows deleted: " + result);
+        }
+
+        // EXECUTE
+
+        try (Connection conn = DriverManager.getConnection(URL);
+             PreparedStatement ps = conn.prepareStatement(SELECT)) {
+            if (ps.execute()) {
+                System.out.println("Execute query and return results set if present: ");
+                ResultSet rs = ps.getResultSet();
+                while (rs.next()) {
+                    System.out.println(rs.getString(1));
+                }
+            }
+        }
+    }
+
+    // BIND VARIABLES
+
+    public static void register(Connection conn, int key, int type, String name) throws SQLException {
+        String sql = "INSERT INTO names VALUES(?, ?, ?)";
+
+        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, key);
+            ps.setInt(2, type);
+            ps.setString(3, name);
+            ps.executeUpdate();
+        }
+    }
+
+    // BATCH STATEMENTS
+
+    public static void register(Connection conn, int firstKey, int type, String... names) throws SQLException {
+        var sql = "INSERT INTO names VALUES(?, ?, ?)";
+        var nextIndex = firstKey;
+
+        try (var ps = conn.prepareStatement(sql)){
+            ps.setInt(2, type);
+
+            for (var name: names) {
+                ps.setInt(1, nextIndex);
+                ps.setString(3, name);
+                ps.addBatch();
+
+                nextIndex++;
+            }
+            int[] result = ps.executeBatch();
+            System.out.println(Arrays.toString(result));
         }
     }
 }
