8888045e530bf5bbac3a88cd0acbb61387bf17f0
StevenD'Hondt <steven.dhondt@kenze.be>
2021-03-21 16:25:50 +0100

    :alembic: Oracle PC - Practice

--- src/be/dog/d/steven/OracleProgrammingComplete/chapter13/app/Shop.java
+++ src/be/dog/d/steven/OracleProgrammingComplete/chapter13/app/Shop.java
@@ -1,6 +1,9 @@
 package be.dog.d.steven.OracleProgrammingComplete.chapter13.app;
 
 import be.dog.d.steven.OracleProgrammingComplete.chapter13.data.ProductFactory;
+import be.dog.d.steven.OracleProgrammingComplete.chapter13.data.Rating;
+
+import java.math.BigDecimal;
 
 public class Shop {
     public static void main(String[] args) {
@@ -11,5 +14,9 @@ public class Shop {
         pf.printProductReport(3);
         pf.printProductReport(4);
 
+        pf.createProduct(5,"Daiquiri", BigDecimal.valueOf(5.99));
+        pf.printProductReport(5);
+        pf.reviewProduct(5, Rating.FIVE_STAR,"Good stuff");
+        pf.printProductReport(5);
     }
 }
